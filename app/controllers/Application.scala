b117432abc0e0c5b43856916aa55b1e9b76f8b98
Fernando Benjamin <benibadboy@hotmail.com>
2015-01-30 15:43:23 +0100

    Finished AMI creation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,10 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+     
+
+
+
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
